9bf9bfe828844b44881c4e741bfed36f9bbc67c4
Ryan Schmidt <skizzerz@skizzerz.net>
2021-04-22 21:09:31 -0700

    Revamp updateRemotePage.php
    I was running into OOM errors when running on enwiki's page list because
    certain things were being kept resident in memory until the script
    completed. Now we break down into subprocesses to handle only one batch
    of updates at a time so that memory is freed more aggressively.
    
    This is also no longer atomic, but instead does upserts for pages that
    exist in the latest run and deletes any stale data (>3 days old) from
    previous runs. This also provides some resilience against crashes so
    that it can pick up where it left off, although currently there is no
    documented way of doing that.

--- maintenance/updateRemotePage.php
+++ maintenance/updateRemotePage.php
@@ -17,160 +17,126 @@ namespace WikiMirror\Maintenance {
 	use MediaWiki\MediaWikiServices;
 	use MediaWiki\Shell\Shell;
 	use ReflectionClass;
-	use WikiMirror\API\SiteInfoResponse;
 	use WikiMirror\Compat\CurlHandler;
-	use WikiMirror\Mirror\Mirror;
 
 	// phpcs:disable MediaWiki.Files.ClassMatchesFilename.WrongCase
 	class UpdateRemotePage extends Maintenance {
+		/** @var array */
+		private $originalOptions;
+
+		/** @var bool */
+		private $isChild = false;
+
+		/** @var int */
+		private $total = -1;
+
+		/** @var array */
+		private $offsets;
+
 		public function __construct() {
 			parent::__construct();
 			$this->requireExtension( 'WikiMirror' );
-			$this->setBatchSize( 500 );
+			$this->setBatchSize( 50000 );
+			$this->setAllowUnregisteredOptions( true );
 			$this->addDescription(
 				'Update locally-stored tracking data about which pages and namespaces exist on the remote wiki'
 			);
+
 			$this->addOption(
 				'dump',
-				'Load page titles from an online dump at the specified URL rather than via the API (faster)',
-				false,
+				'URL or file path of the dump containing valid page names',
+				true,
 				true
 			);
 		}
 
 		/**
+		 * @inheritDoc
+		 */
+		public function finalSetup() {
+			// save a pristine copy of $this->mOptions before our parent munges it
+			$this->originalOptions = $this->mOptions;
+			parent::finalSetup();
+		}
+
+		/**
 		 * Update remote_page with data from remote wiki
-		 * @return bool true on success, false on failure
+		 * @return bool
 		 * @throws Exception
 		 */
 		public function execute() {
-			if ( $this->hasOption( 'dump' ) ) {
-				return $this->fetchFromDump();
+			// delete records older than 3 days
+			$stale = wfTimestamp( TS_MW, time() - 259200 );
+			$batchSize = $this->getBatchSize();
+			$skip = $this->getOption( 'skip' );
+			$count = $this->getOption( 'count' );
+
+			if ( $batchSize > 0 && $skip !== null && $count !== null ) {
+				// we're a child process executing in batched mode
+				$this->isChild = true;
 			} else {
-				return $this->fetchFromAPI();
-			}
-		}
-
-		private function fetchFromAPI() {
-			$config = $this->getConfig();
-			$interwikiLookup = MediaWikiServices::getInstance()->getInterwikiLookup();
-			$requestFactory = MediaWikiServices::getInstance()->getHttpRequestFactory();
-
-			$remoteWiki = $config->get( 'WikiMirrorRemote' );
-			if ( $remoteWiki === null ) {
-				$this->error( '$wgWikiRemote not configured in LocalSettings.php.' );
-				return false;
+				// non-batched mode or we're the parent
+				$count = 0;
+				$skip = 0;
 			}
 
-			$interwiki = $interwikiLookup->fetch( $remoteWiki );
-			if ( $interwiki === null || $interwiki === false ) {
-				// invalid interwiki configuration
-				$this->error( 'Invalid interwiki configuration for $wgWikiMirrorRemote.' );
-				return false;
+			$path = $this->getOption( 'dump' );
+			$haveTempFile = false;
+			if ( wfParseUrl( $path ) !== false ) {
+				// we were passed a URL to download;
+				// this will generate a temporary file we need to clean up
+				$path = $this->downloadDump( $path );
+				$haveTempFile = true;
 			}
 
-			/** @var Mirror $mirror */
-			$mirror = MediaWikiServices::getInstance()->get( 'Mirror' );
-			$status = $mirror->getCachedSiteInfo();
-			if ( !$status->isOK() ) {
-				$this->error( 'Unable to retrieve remote site information.' );
-				return false;
+			try {
+				if ( !$this->isChild ) {
+					$this->outputChanneled( 'Calculating dataset size...' );
+					$this->calculateTotalAndOffsets( $path );
+					$this->outputChanneled( 'Loading data...' );
+				}
+
+				if ( $batchSize > 0 && !$this->isChild ) {
+					$this->spawnSubprocesses( $path );
+				} else {
+					$this->fetchFromDump( $path, $skip, $count );
+				}
+			} finally {
+				// phpcs:disable Generic.PHP.NoSilencedErrors.Discouraged
+				if ( $haveTempFile ) {
+					@unlink( $path );
+				}
 			}
 
-			/** @var SiteInfoResponse $siteInfo */
-			$siteInfo = $status->getValue();
-
-			$apiUrl = $interwiki->getAPI();
-			$params = [
-				'format' => 'json',
-				'formatversion' => 2,
-				'errorformat' => 'plaintext',
-				'errorlang' => 'en',
-				'action' => 'query',
-				'list' => 'allpages',
-				'aplimit' => 'max'
-			];
-
-			// start a transaction
-			$db = $this->getDB( DB_MASTER );
-			$db->begin( __METHOD__ );
-
-			// delete existing data
-			$db->delete( 'remote_page', '1=1', __METHOD__ );
-
-			// number of pages processed thus far
-			$count = 0;
-
-			foreach ( $siteInfo->namespaces as $nsData ) {
-				$nsName = $nsData['id'] === 0 ? '(Main)' : $nsData['name'];
-				$this->outputChanneled( 'Retrieving namespace ' . $nsName );
-				$params['apnamespace'] = $nsData['id'];
-
-				do {
-					$res = $requestFactory->get( wfAppendQuery( $apiUrl, $params ), [], __METHOD__ );
-					$now = wfTimestampNow();
-
-					if ( $res === null ) {
-						// API error
-						$this->error( 'Unable to retrieve remote page data. Rolling back (this may take a while).' );
-						$db->rollback( __METHOD__ );
-						return false;
-					}
-
-					$data = json_decode( $res, true );
-					if ( array_key_exists( 'continue', $data ) ) {
-						$params['apcontinue'] = $data['continue']['apcontinue'];
-						$done = false;
-					} else {
-						$done = true;
-					}
-
-					foreach ( $data['query']['allpages'] as $page ) {
-						$db->insert( 'remote_page', [
-							'rp_namespace' => $page['ns'],
-							'rp_title' => str_replace( ' ', '_', $page['title'] ),
-							'rp_updated' => $now
-						], __METHOD__ );
-
-						if ( ++$count % 500 === 0 ) {
-							$this->outputChanneled( $count );
-						}
-					}
-				} while ( !$done );
+			if ( !$this->isChild ) {
+				$this->outputChanneled( 'Data loaded successfully!' );
+				$this->deleteStaleRecords( $stale );
 			}
-
-			// commit
-			$db->commit( __METHOD__ );
 
 			return true;
 		}
 
 		/**
-		 * Fetches a dump from the passed-in URL. The dump should be a TSV
-		 * with the namespace id as the first column and page name as second column (in db key format).
-		 * The dump can be gzipped, and will be extracted before being loaded.
+		 * Downloads a dump file from the passed-in URL, displaying a progress meter
+		 * to the user.
 		 *
 		 * Example URL: https://dumps.wikimedia.org/enwiki/20210320/enwiki-20210320-all-titles.gz
 		 *
-		 * @return bool True on success, false on failure
+		 * @param string $remote URL to download dump from
+		 * @return string Full path of downloaded dump
 		 * @throws Exception on error
 		 */
-		private function fetchFromDump() {
+		private function downloadDump( string $remote ) {
 			$requestFactory = MediaWikiServices::getInstance()->getHttpRequestFactory();
-			$remote = $this->getOption( 'dump' );
-
 			$tmpFile = tempnam( wfTempDir(), 'wmr' );
 
-			// MediaWiki is broken and doesn't actually pass the timeout to Guzzle correctly,
-			// so the default timeout is always used. Fix that here.
-			$handler = new CurlHandler( [
-				CURLOPT_TIMEOUT => 0
-			] );
-
-			$fh = false;
-			$now = wfTimestampNow();
-
 			try {
+				// MediaWiki is broken and doesn't actually pass the timeout to Guzzle correctly,
+				// so the default timeout is always used. Fix that here.
+				$handler = new CurlHandler( [
+					CURLOPT_TIMEOUT => 0
+				] );
+
 				$http = $requestFactory->create( $remote, [
 					'sink' => $tmpFile,
 					'handler' => $handler
@@ -188,66 +154,185 @@ namespace WikiMirror\Maintenance {
 					// someone decided to change the default http engine
 					// this is deprecated yet still supported in core, but unsupported here
 					// we can remove this once the ability to configure the http engine is removed
-					$this->error( 'The HTTP engine is not Guzzle; do not modify the default engine' );
-					return false;
+					$this->fatalError( 'The HTTP engine is not Guzzle; do not modify the default engine' );
 				}
 
 				$this->outputChanneled( "Downloading $remote..." );
 				$res = $http->execute();
 				if ( !$res->isOK() ) {
-					$this->error( $res->getMessage()->text() );
-					return false;
+					$this->fatalError( $res->getMessage()->text() );
 				}
 
 				$this->outputChanneled( 'Download complete!' );
+			} catch ( Exception $e ) {
+				// phpcs:disable Generic.PHP.NoSilencedErrors.Discouraged
+				@unlink( $tmpFile );
+				throw $e;
+			}
 
-				// start a transaction
-				$db = $this->getDB( DB_MASTER );
-				$db->begin( __METHOD__ );
+			return $tmpFile;
+		}
 
-				// delete existing data
-				$db->delete( 'remote_page', '1=1', __METHOD__ );
+		/**
+		 * Populates $this->total and $this->offsets
+		 *
+		 * @param string $path File path to dump
+		 */
+		private function calculateTotalAndOffsets( $path ) {
+			$batchSize = $this->getBatchSize();
+			$offsets = [ 0 => 0 ];
+			$count = 0;
 
-				// write new data to db
-				$this->outputChanneled( 'Loading data...' );
-				$fh = gzopen( $tmpFile, 'rb' );
-				if ( $fh === false ) {
-					$this->error( 'Could not open downloaded file for reading' );
-					return false;
+			$fh = gzopen( $path, 'rb' );
+			if ( $fh === false ) {
+				$this->fatalError( 'Could not open dump file for reading' );
+			}
+
+			while ( !gzeof( $fh ) ) {
+				$line = gzgets( $fh );
+				if ( $line === false || $line === '' ) {
+					continue;
 				}
 
-				$count = 0;
+				++$count;
+
+				if ( $count % $batchSize === 0 ) {
+					$offsets[$count] = gztell( $fh );
+				}
+			}
+
+			$this->total = $count;
+			$this->offsets = $offsets;
+		}
+
+		/**
+		 * Spawns subprocesses to handle the batches. This reduces memory pressure
+		 * on the main PHP process for very large dumps, as some memory is not freed
+		 * until the script completely finishes executing.
+		 *
+		 * @param string $path Path to dump file
+		 */
+		private function spawnSubprocesses( $path ) {
+			$batchSize = $this->getBatchSize();
+			$specialOpts = [ 'skip', 'count', 'total' ];
+
+			$opts = $this->originalOptions;
+			$opts['batch-size'] = $batchSize;
+			$opts['dump'] = $path;
+			$opts['total'] = $this->total;
+
+			for ( $cur = 0; $cur < $this->total; $cur += $batchSize ) {
+				$opts['skip'] = $this->offsets[$cur];
+				$opts['count'] = $cur;
+				$parameters = [];
+				foreach ( $opts as $k => $v ) {
+					if ( in_array( $k, $specialOpts ) ) {
+						$parameters[] = "--{$k}={$v}";
+					} else {
+						$parameters[] = "--{$k}";
+						$parameters[] = $v;
+					}
+				}
+
+				// explicitly pass the same php binary executing the current script, since $wgPhpCli is
+				// typically never set in LocalSettings.php (which is what makeScriptCommand defaults to);
+				// this makes the default completely unusable on Windows and exotic setups
+				$command = Shell::makeScriptCommand( __FILE__, $parameters, [ 'php' => PHP_BINARY ] );
+
+				// don't actually execute through Shell because we want to have stdout print immediately
+				// in the subprocesses, and we don't want to limit via cgroups, firejail, etc.
+				// passthru() does exactly what we want it to do, so use that directly
+				// phpcs:disable MediaWiki.Usage.ForbiddenFunctions.passthru
+				passthru( $command->getCommandString(), $exitCode );
+				if ( $exitCode !== 0 ) {
+					// subcommand failed, abort
+					$this->fatalError( 'Aborting data load due to subcommand failure.' );
+				}
+			}
+		}
+
+		/**
+		 * Fetches a dump from the passed-in file. The dump should be a TSV
+		 * with the namespace id as the first column and page name as second column (in db key format).
+		 * The dump can be gzipped, and will be extracted before being loaded.
+		 *
+		 * @param string $path File (possibly gzipped) containing the TSV dump
+		 * @param int $skip Byte offset to skip to in the dump file
+		 * @param int $count Number of records retrieved by previous executions
+		 * @throws Exception on error
+		 */
+		private function fetchFromDump( string $path, $skip, $count ) {
+			$now = wfTimestampNow();
+
+			// check if we need to execute in batched mode
+			$batchSize = $this->getBatchSize();
+			$total = $this->getOption( 'total', $this->total );
+			$width = intval( log10( $total ) ) + 1;
+
+			if ( $total === -1 ) {
+				$this->fatalError( 'Missing total' );
+			}
+
+			try {
+				$db = $this->getDB( DB_MASTER );
+
+				// write new data to db
+				$fh = gzopen( $path, 'rb' );
+				if ( $fh === false ) {
+					$this->fatalError( 'Could not open dump file for reading' );
+				}
+
+				if ( $skip > 0 ) {
+					gzseek( $fh, $skip, SEEK_SET );
+				}
+
 				while ( !gzeof( $fh ) ) {
 					$line = gzgets( $fh );
 					if ( $line === false || $line === '' ) {
-						break;
+						continue;
 					}
 
+					++$count;
 					list( $nsId, $title ) = explode( "\t", $line );
-					$db->insert( 'remote_page', [
+					$db->upsert( 'remote_page', [
 						'rp_namespace' => $nsId,
 						'rp_title' => trim( $title ),
 						'rp_updated' => $now
+					], [
+						[ 'rp_namespace', 'rp_title' ]
+					], [
+						'rp_updated' => $now
 					], __METHOD__ );
 
-					if ( ++$count % 1000 === 0 ) {
-						$this->outputChanneled( $count );
+					$percent = $count / $total * 100;
+					$this->outputChanneled( sprintf( "\r[ %{$width}d / %d ] %3d%% ",
+						$count, $total, $percent ), 'progress' );
+
+					if ( $batchSize > 0 && $count % $batchSize === 0 ) {
+						break;
 					}
 				}
-
-				// commit transaction
-				$db->commit( __METHOD__ );
-				$this->outputChanneled( 'Load complete!' );
 			} finally {
 				// phpcs:disable Generic.PHP.NoSilencedErrors.Discouraged
 				if ( $fh !== false ) {
 					@gzclose( $fh );
 				}
-
-				@unlink( $tmpFile );
 			}
+		}
 
-			return true;
+		/**
+		 * Delete records from cache that are older than the specified timestamp
+		 *
+		 * @param string $ts Timestamp (in TS_MW format) of cutoff
+		 */
+		private function deleteStaleRecords( $ts ) {
+			$db = $this->getDB( DB_MASTER );
+			$db->delete( 'remote_page', [
+				'rp_updated < ' . $ts
+			], __METHOD__ );
+
+			$numRows = $db->affectedRows();
+			$this->outputChanneled( "$numRows stale records deleted!" );
 		}
 
 		/**
@@ -279,9 +364,9 @@ namespace WikiMirror\Maintenance {
 				$current = sprintf( '%.2d GB', $dlBytes / 1000000000 );
 			}
 
-			// trailing space in output is intentional so that blocky cursors don't cover up
-			// the final dot in the ellipses.
-			$this->outputChanneled( "\r$current / $total... ", 'progress' );
+			// trailing spaces in output are intentional so that blocky cursors don't cover up
+			// the final dot in the ellipses and to get rid of leftover chars should our output length shrink
+			$this->outputChanneled( "\r$current / $total...            ", 'progress' );
 		}
 	}
 }
