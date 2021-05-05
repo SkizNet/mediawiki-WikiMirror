ebcbad6f5cba6eb2660e01ba38a0da2bbdba8857
Ryan Schmidt <skizzerz@skizzerz.net>
2021-05-04 19:45:46 -0700

    Add phan config file

---
+++ .phan/config.php
@@ -0,0 +1,20 @@
+<?php
+
+// use phan config shipped with mediawiki core
+$cfg = require __DIR__ . '/../../../vendor/mediawiki/mediawiki-phan-config/src/config.php';
+
+$cfg['directory_list'] = array_merge(
+	$cfg['directory_list'],
+	[
+		'../../extensions/VisualEditor',
+	]
+);
+
+$cfg['exclude_analysis_directory_list'] = array_merge(
+	$cfg['exclude_analysis_directory_list'],
+	[
+		'../../extensions/VisualEditor',
+	]
+);
+
+return $cfg;
